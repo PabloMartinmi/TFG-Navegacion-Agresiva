adb12754ec400f715a56756d081ceaca5a68e162
PabloMartinmi <97103523+PabloMartinmi@users.noreply.github.com>
2026-05-23 11:56:13 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import datetime
+import json
+
+# Imported scripts
+from sql_request import sql_request
+from test_collisions import check_collisions
+
+# Get config values
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+init_date = datetime.datetime.strptime(config['init_date'], "%Y-%m-%d %H:%M:%S")
+end_date = datetime.datetime.strptime(config['end_date'], "%Y-%m-%d %H:%M:%S")
+time_range = config['time_range']
+ell_factor = config['ell_factor']
+min_vel = config['min_vel']
+
+# Query the db with the parameters introduced by the user
+query = sql_request(init_date, end_date, time_range, min_vel)
+
+# Execute the script that calculates collisions
+check_collisions(query, min_vel, ell_factor) 
+
+# To run map:
+# python map.py "2020-01-01_13-45-00_2020-01-01_14-15-00_15.txt"
